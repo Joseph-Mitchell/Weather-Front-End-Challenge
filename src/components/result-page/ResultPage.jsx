8299cb2e64f18d0937e6780195d81e0fe1d4094d
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-10 23:54:55 +0100

    FEAT: static add to favourites button

--- src/components/result-page/ResultPage.jsx
+++ src/components/result-page/ResultPage.jsx
@@ -1,13 +1,19 @@
 import WeatherCard from "./WeatherCard.jsx";
 import WeatherToday from "./WeatherToday.jsx";
 
+import bookmark from "../../assets/icons/bookmark-star-fill.svg";
+
 const ResultPage = () => {
     return (
         <div className="container h-100 pt-1 text-center">
             <h1>Telling you about...</h1>
             <h1>Place name</h1>
-            <p><small>Click to add to favourites</small></p>
+            <p>
+                <a href=""><img src={bookmark} height="15px" /></a>&nbsp;
+                <small>Click to add to favourites</small>
+            </p>
             <WeatherToday />
+            <hr />
             <div className="row">
                 <WeatherCard />
                 <WeatherCard />
