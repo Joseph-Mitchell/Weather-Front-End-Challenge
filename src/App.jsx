89010c1c66d233f0cdd2673fe0f30112b74e3159
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-10 17:36:51 +0100

    FEAT: static header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/footer/Footer.jsx";
+import Header from "./components/header/Header.jsx";
 import SearchPage from "./components/search-page/SearchPage.jsx";
 
 const App = () => {
     return (
         <>
+            <Header />
             <SearchPage />
             <Footer />
         </>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="position-sticky start-0 top-0 vw-100 bg-secondary-subtle d-flex align-items-center justify-content-between px-3 vh-50">
+            <a href="/"><img className="p-1" src="./src/assets/images/logo.png" height="60px" ></img></a>
+            <a href="/"><img className="p-1 bi" src="./src/assets/icons/list.svg" height="40px" ></img></a>
+        </header>
+    );
+};
+export default Header;
