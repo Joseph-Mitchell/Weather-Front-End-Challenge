84a582afd4f105bd63cd3558c7780a8ea4bccf4a
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-11 18:05:28 +0100

    FEAT: search page styling

--- src/components/search-page/SearchPage.jsx
+++ src/components/search-page/SearchPage.jsx
@@ -1,11 +1,10 @@
 const SearchPage = () => {
     return (
-        <div className="container h-100 d-flex align-items-center justify-content-center">
+        <div className="container page text-light d-flex align-items-center justify-content-center">
             <form className="text-center" action="/search/test" style={{ marginTop: -125 }}>
-                <h1>Tell me about...</h1>
-                <input placeholder="Location name..."></input>
-                <br />
-                <button>Search</button>
+                <h1 className="m-0">Tell me about...</h1>
+                <input className="form-control p-2 my-4 fs-5" placeholder="Location name..."></input>
+                <button type="submit" className="btn btn-primary">Search</button>
             </form>
         </div>
     );
