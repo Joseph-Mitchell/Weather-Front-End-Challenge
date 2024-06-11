f42411a6dbb61da9f3de020b25a612c22950e0d4
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-11 16:29:52 +0100

    FEAT: added background image to pages

--- src/components/favourites-page/FavouritesPage.jsx
+++ src/components/favourites-page/FavouritesPage.jsx
@@ -2,7 +2,7 @@ import FavouritesList from "./FavouritesList.jsx";
 
 const FavouritesPage = () => {
     return (
-        <div className="container h-100 text-center">
+        <div className="container page">
             <h1>Favourite Locations</h1>
             <p>Click <img src="/assets/icons/favourite-icons/bookmark-star-fill.svg" /> to remove favourite location</p>
             <p>Click location name to view it's forecast</p>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import NavButtons from "./NavButtons.jsx";
 const Header = () => {
     return (
         <header className="position-sticky start-0 top-0 vw-100 bg-secondary-subtle d-flex align-items-center justify-content-between px-3 vh-50">
-            <a href="/"><img className="p-1" src="/assets/logo.png" height="60px" ></img></a>
+            <a href="/"><img className="p-1" src="/assets/images/logo.png" height="60px" ></img></a>
             <NavButtons />
             <a href="#offCanvasMenu" data-bs-toggle="offcanvas"><img className="p-1 bi" src="/assets/icons/misc-icons/list.svg" height="40px" ></img></a>
         </header>
