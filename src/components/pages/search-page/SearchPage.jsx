e53c305933543f5aae4ac2eb574999e3cff569dd
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-13 00:18:19 +0100

    FEAT: added location suggestions to search form

--- src/components/pages/search-page/SearchPage.jsx
+++ src/components/pages/search-page/SearchPage.jsx
@@ -1,10 +1,25 @@
 const SearchPage = () => {
+
+    const submitSearch = e => {
+        e.preventDefault();
+
+    };
+
     return (
         <div className="d-flex | align-items-center justify-content-center | h-100">
-            <form className="text-center" action="/search/test">
+            <form className="text-center" onSubmit={submitSearch}>
                 <h1 className="m-0">Tell me about...</h1>
-                <input className="form-control p-2 my-4 fs-5" placeholder="Location name..."></input>
-                <button type="submit" className="btn btn-primary">Search</button>
+                <div className="row | d-flex | align-items-start | mt-4">
+                    <div className="list-group | text-start | col">
+                        <input className="list-group-item | p-2 | fs-5" placeholder="Location name..." />
+                        <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+                        <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+                        <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+                        <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+                    </div>
+                    <button className="btn btn-primary | col-auto" type="submit"><i class="bi-search fs-4" /></button>
+
+                </div>
             </form>
         </div>
     );
