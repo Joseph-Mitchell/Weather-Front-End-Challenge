15c5afb8ad5864d1430db6ad0ec5426bba0d0579
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-12 21:07:41 +0100

    FIX: Favourite location link underline on hover

--- src/components/favourites-page/Favourite.jsx
+++ src/components/favourites-page/Favourite.jsx
@@ -2,7 +2,7 @@ const Favourite = () => {
     return (
         <li className="list-group-item | bg-transparent">
             <a className="me-3" href=""><i className="bi-bookmark-star-fill text-light" /></a>
-            <a className="link-light link-underline link-underline-opacity-0 link-underline-opacity-0-hover" href="/search/test">Location</a>
+            <a className="link-light link-underline link-offset-1 link-underline-opacity-0 link-underline-opacity-100-hover" href="/search/test">Location</a>
         </li>
     );
 };
--- src/components/favourites-page/FavouritesPage.jsx
+++ src/components/favourites-page/FavouritesPage.jsx
@@ -5,7 +5,7 @@ const FavouritesPage = () => {
         <>
             <h2 className="mt-5">Telling you about...</h2>
             <h1>Favourite Locations</h1>
-            <p className="text-start">
+            <p className="text-start | ms-3">
                 Click <i className="bi-bookmark-star-fill text-light" /> to remove favourite location
                 <br />
                 Click location name to view it's forecast
