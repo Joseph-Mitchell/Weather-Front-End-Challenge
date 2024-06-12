c75666a4771740c9249d7f183b6a6215ea4139db
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-12 22:28:07 +0100

    FEAT: styled login modal

--- src/components/ui/LoginModal.jsx
+++ src/components/ui/LoginModal.jsx
@@ -1,19 +1,19 @@
 const LoginModal = () => {
     return (
         <div className="modal" id="loginModal">
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <form className="modal-body text-center">
-                        <h1>Log-In / Sign-Up</h1>
+            <div className="modal-dialog modal-dialog-centered" id="login-modal">
+                <div className="modal-content bg-secondary-subtle">
+                    <form className="modal-body text-center fs-5">
+                        <h1 className="mb-4">Log-In / Sign-Up</h1>
                         <label>E-mail</label><br />
-                        <input type="email" placeholder="email@example.com" />
-                        <br /><br />
+                        <input className="mb-4 py-1 px-2 | border border-1 rounded border-dark-subtle" type="email" placeholder="email@example.com" />
+                        <br />
                         <label>Password</label><br />
-                        <input type="password" placeholder="password" />
+                        <input className="py-1 px-2 | border border-1 rounded border-dark-subtle" type="password" placeholder="Password" />
                         <br /><br />
-                        <div className="d-flex justify-content-center">
-                            <button className="mx-2" type="submit">Log-In</button>
-                            <button className="mx-2" type="submit">Sign-Up</button>
+                        <div className="d-flex | justify-content-evenly | mt-3 mb-4">
+                            <button className="btn btn-lg btn-primary | w-25" type="submit">Log-In</button>
+                            <button className="btn btn-lg btn-outline-primary | w-25" type="submit">Sign-Up</button>
                         </div>
                     </form>
                 </div>
