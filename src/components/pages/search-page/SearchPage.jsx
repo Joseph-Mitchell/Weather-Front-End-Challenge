f4174fed38b14c336178869e069b93fce8cf5322
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-13 01:52:37 +0100

    FEAT: search suggestion popup interaction

--- src/components/pages/search-page/SearchPage.jsx
+++ src/components/pages/search-page/SearchPage.jsx
@@ -1,25 +1,37 @@
+import { useLayoutEffect } from "react";
+
 const SearchPage = () => {
 
-    const submitSearch = e => {
-        e.preventDefault();
+    useLayoutEffect(() => {
+        const list = document.getElementById("location-suggestions");
+        const searchBarRect = document.getElementById("search-bar").getBoundingClientRect();
 
+        list.style.width = `${searchBarRect.right - searchBarRect.left}px`;
+        list.style.left = searchBarRect.left;
+        list.style.top = searchBarRect.bottom;
+    });
+
+    const hideSearches = (e) => {
+        document.getElementById("location-suggestions").classList.add("d-none");
+    };
+    const showSearches = (e) => {
+        document.getElementById("location-suggestions").classList.remove("d-none");
     };
 
     return (
         <div className="d-flex | align-items-center justify-content-center | h-100">
-            <form className="text-center" onSubmit={submitSearch}>
+            <form className="text-center">
                 <h1 className="m-0">Tell me about...</h1>
-                <div className="row | d-flex | align-items-start | mt-4">
-                    <div className="list-group | text-start | col">
-                        <input className="list-group-item | p-2 | fs-5" placeholder="Location name..." />
-                        <a href="#" class="list-group-item list-group-item-action">A second link item</a>
-                        <a href="#" class="list-group-item list-group-item-action">A third link item</a>
-                        <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-                        <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-                    </div>
-                    <button className="btn btn-primary | col-auto" type="submit"><i class="bi-search fs-4" /></button>
-
+                <div className="d-flex">
+                    <input className="d-block p-2 mt-4 fs-5 flex-fill" id="search-bar" placeholder="Location name..." onBlur={hideSearches} onFocus={showSearches} />
                 </div>
+                <div className="list-group | d-none | position-absolute | text-start | border-2 rounded-0" id="location-suggestions">
+                    <a href="#" className="list-group-item list-group-item-action">A second link item</a>
+                    <a href="#" className="list-group-item list-group-item-action">A third link item</a>
+                    <a href="#" className="list-group-item list-group-item-action">A fourth link item</a>
+                    <a href="#" className="list-group-item list-group-item-action">A fourth link item</a>
+                </div>
+                <button type="submit" className="btn btn-primary mt-4">Search</button>
             </form>
         </div>
     );
